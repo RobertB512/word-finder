90e4bbd2f9a0dd2a3f2c4c0045b9425ddc9a99a2
RobertB512 <rwbcool@gmail.com>
2023-07-25 14:21:42 -0700

    Add only real words to list

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,42 @@
-const addUserWord = guessedWord => {
+const addUserWord = async guessedWord => {
 	const guessedWordsSection = document.querySelector(".guessed-words-section");
 
 	if (guessedWord) {
 		console.log("valid word");
-		let goodWord = document.createElement("p")
-    goodWord.classList.add("good-word")
-    goodWord.textContent = guessedWord;
-    console.log("goodWord is", goodWord)
+		const userWord = guessedWord;
+		console.log("guessedWord is", userWord);
+		let goodWord = document.createElement("p");
+		goodWord.classList.add("good-word");
+		goodWord.textContent = userWord;
+		console.log("goodWord is", goodWord);
 		guessedWordsSection.append(goodWord);
 	}
 };
 
-const getWordFromUser = () => {
+const checkWord = async wordToVerify => {
+	const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordToVerify}`;
+	try {
+		const response = await fetch(url);
+		if (response.ok) {
+			const data = await response.json();
+			const theWord = await data[0].word;
+			console.log("here is the word:", theWord);
+			addUserWord(theWord);
+		} else {
+			console.log("ERROR: might not be a valid word");
+		}
+	} catch (error) {
+		console.log("failed fetch:", error);
+	}
+};
+
+const getWordFromUser = async () => {
 	const wordEntryForm = document.querySelector(".word-entry-form");
 	const wordEntryInput = document.querySelector(".word-entry-input");
 
 	wordEntryForm.addEventListener("submit", e => {
 		e.preventDefault();
-		console.log(wordEntryInput.value);
-		addUserWord(wordEntryInput.value);
+		checkWord(wordEntryInput.value);
 	});
 };
 
